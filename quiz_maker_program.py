a50592cb710609dd287566bda407ab2a1b091e61
KirbyClaro <kirbychristianclaro@gmail.com>
2025-04-09 13:43:34 +0800

    added question input functionality with strip and printed the output with validation in the main()

--- quiz_maker_program.py
+++ quiz_maker_program.py
@@ -4,9 +4,13 @@
 #to a text file. Ask another question until 
 #the user chose to exit.
 
+def get_user_question():
+    return input("Enter your question: ").strip()
 def main():
     print("Welcome to Quiz Creator!")
     print("Press Control + C to quit.")
+    question = get_user_question()
+    print(f"You entered: {question}")
 
 if __name__ == "__main__":
     main()
