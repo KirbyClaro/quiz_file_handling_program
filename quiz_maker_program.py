8e8fb3fa865722a1ebb73171154f3bfa8c73ea11
KirbyClaro <kirbychristianclaro@gmail.com>
2025-04-09 13:31:23 +0800

    Creating Initial set up defined main program and added print output to show what the program is about

--- quiz_maker_program.py
+++ quiz_maker_program.py
@@ -3,3 +3,10 @@
 #and the correct answer. Write the collected data 
 #to a text file. Ask another question until 
 #the user chose to exit.
+
+def main():
+    print("Welcome to Quiz Creator!")
+    print("Press Control + C to quit.")
+
+if __name__ == "__main__":
+    main()
