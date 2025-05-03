20c34161902f30aa3d18e32a8be796c6ac1c295a
KirbyClaro <kirbychristianclaro@gmail.com>
2025-05-03 21:44:36 +0800

    added the file searcher to get the filename checks in the same folder also using if statement with strip and endswith functions

--- quiz_reader_program.py
+++ quiz_reader_program.py
@@ -3,6 +3,15 @@
 #question and check if the answer is correct.
 
 #ask user for file name
+def get_filename():
+    while True:
+        filename = input("Enter the filename to load question").strip()
+        if filename:
+            if not filename.endswith(".txt"):
+                filename  += ".txt"
+            return filename
+        print("Filename cannot be empty!")
+        return
 #access the file and read it
 #display the questions
 #accept and check user answer
