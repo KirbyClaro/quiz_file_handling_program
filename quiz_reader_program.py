96bd70677145f30914b638045aedaf68b9c1cbe5
KirbyClaro <kirbychristianclaro@gmail.com>
2025-05-03 21:40:55 +0800

    added the flow of the program

--- quiz_reader_program.py
+++ quiz_reader_program.py
@@ -1,3 +1,10 @@
 #Create the Quiz program that read the output file of the Quiz Creator. 
 #The user will answer the randomly selected 
 #question and check if the answer is correct.
+
+#ask user for file name
+#access the file and read it
+#display the questions
+#accept and check user answer
+#run quiz and score
+#main function with replay option
